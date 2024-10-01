a910551cf77536f03073172a81c754dbfd357fdb
flaska vitek <flaska.vitek>
2024-10-01 12:24:15 +0200

    přidání rekurze

--- obsah_obd.py
+++ obsah_obd.py
@@ -1,7 +1,11 @@
-a = int(input("Zadej stranu a: "))
-b = int(input("Zadej stranu b: "))
-if a > 0 and b > 0:
-    obsah = a * b
-    print(f"Obsah obdelníku o stranách o velikosti {a} cm a {b} cm je {obsah}.")
-else:
-    print("Error - špatně zadané hodnoty")
+def obsah_obdelniku():
+    a = int(input("Zadej stranu a: "))
+    b = int(input("Zadej stranu b: "))
+    if a > 0 and b > 0:
+        obsah = a * b
+        print(f"Obsah je {obsah}.")
+    else:
+        print("Chyba, opakuj zadání.")
+        obsah_obdelniku()
+
+obsah_obdelniku()
