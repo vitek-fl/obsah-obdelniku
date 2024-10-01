871d0632fa43c0cf86124f5a917a24fb9b77bafb
flaska vitek <flaska.vitek>
2024-10-01 12:14:39 +0200

    přidání chyby

--- obsah_obd.py
+++ obsah_obd.py
@@ -1,5 +1,7 @@
 a = int(input("Zadej stranu a: "))
 b = int(input("Zadej stranu b: "))
-obsah = a * b
-print(f"Obsah obdelníku o stranách o velikosti {a} cm a {b} cm je {obsah}.")
-
+if a > 0 and b > 0:
+    obsah = a * b
+    print(f"Obsah obdelníku o stranách o velikosti {a} cm a {b} cm je {obsah}.")
+else:
+    print("Error - špatně zadané hodnoty")
